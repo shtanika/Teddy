7a3bca8dba0e3be06b894b7a3f57ee94b2a57cd0
Tanika Shamalia <tanika.shamalia27@myhunter.cuny.edu>
2025-03-21 21:59:38 -0400

    added pages

---
+++ client/src/app/(pages)/ai-insights/page.tsx
@@ -0,0 +1,7 @@
+export default function AIInsightsPage() {
+  return (
+    <div>
+      <h1>AI Insights</h1>
+    </div>
+  );
+}
---
+++ client/src/app/(pages)/goals/page.tsx
@@ -0,0 +1,7 @@
+export default function GoalsPage() {
+  return (
+    <div>
+      <h1>Goals</h1>
+    </div>
+  );
+}
