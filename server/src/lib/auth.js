b2fc9927b8d60d8c389fc909e40605d55e1367c0
Tanika Shamalia <tanika.shamalia27@myhunter.cuny.edu>
2025-05-09 12:21:43 -0400

    removed console logs

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -3,10 +3,6 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import 'dotenv/config';
 
-
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
-
 const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
