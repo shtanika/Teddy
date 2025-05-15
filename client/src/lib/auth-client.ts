683c07f498f5d4120c3193d7235487d31650591a
Tanika Shamalia <56367762+shtanika@users.noreply.github.com>
2025-05-14 20:05:15 -0400

    fixing hardcoded urls (#8)

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:5000",
+    baseURL: process.env.BACKEND_URL,
     fetchOptions: {
         credentials: 'include'
     }
