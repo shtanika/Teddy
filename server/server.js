1c3e5712bece40d977346371c61e5cb57899bd11
Tanika Shamalia <tanika.shamalia27@myhunter.cuny.edu>
2025-05-14 20:04:33 -0400

    fixing hardcoded urls

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // NextJS client URL
+  origin: process.env.FRONTEND_URL, // NextJS client URL
   credentials: true, // Allow credentials (cookies, authorization headers)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
