8f9bb824f4b3f7674789f78060f3a673953261ab
Kian Mahd Soltani <kianmahdsoltani@Kians-MacBook-Pro.local>
2025-05-19 15:47:11 -0400

    dashboard display is working

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,14 @@
 import type { NextConfig } from "next";
+import dotenv from 'dotenv'
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../server/.env") });
+
+const backendUrl = process.env.BACKEND_URL;
+
+if (!backendUrl) {
+  throw new Error("missing BACKEND_URL in .env file");
+}
 
 const nextConfig: NextConfig = {
   async rewrites() {
